9df14a269becb167d2e44adce13764a248909ae7
Miroslav Stastny <mistastn@microsoft.com>
2019-01-31 19:52:20 +0100

    Add MongoDB client

--- SaveStats/index.js
+++ SaveStats/index.js
@@ -1,13 +1,33 @@
+const mongodb = require('mongodb')
+
 module.exports = async function (context, req) {
+    const mongodbUri = process.env["MONGODB_URI"];
+    const mongodbUser = process.env["MONGODB_USER"];
+    const mongodbPass = process.env["MONGODB_PASS"];
+
     context.log('JavaScript HTTP trigger function processed a request.');
+    context.log('DB:', mongodbUri);
 
     if (req.query.name || (req.body && req.body.name)) {
-        context.log(process.env["MONGODB_URL"]);
 
         context.res = {
             // status: 200, /* Defaults to 200 */
             body: "Hello " + (req.query.name || req.body.name)
         };
+
+        const mongoClient = await mongodb.MongoClient.connect(mongodbUri, {
+            auth: {
+                user: mongodbUser,
+                password: mongodbPass
+            }
+        });
+
+        context.log('connected to mongodb')
+        await mongoClient.db('stardust').collection('stats').insertOne({
+            name: req.body.name
+        });
+        context.log('document inserted')
+        await mongoClient.close()
     }
     else {
         context.res = {
