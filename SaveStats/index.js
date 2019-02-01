f60666a51e180bbc6b59ab4ee96fab38d36276ce
Miroslav Stastny <mistastn@microsoft.com>
2019-02-01 13:23:34 +0100

    Store real PR data

--- SaveStats/index.js
+++ SaveStats/index.js
@@ -1,4 +1,17 @@
-const mongodb = require('mongodb')
+const mongodb = require('mongodb');
+const _ = require('lodash');
+
+function checkDataContainsFields(data, requiredFields) {
+    for (let i = 0; i < requiredFields.length; i++) {
+        const key = requiredFields[i]
+        if (data[key] === undefined) {
+            return `${key} field not set`
+        }
+    }
+}
+
+const mandatoryFields = ['sha', 'branch', 'build', 'bundleSize', 'ts'];
+const optionalFields = ['tag', 'pr'];
 
 module.exports = async function (context, req) {
     const mongodbUri = process.env["MONGODB_URI"];
@@ -12,40 +25,34 @@ module.exports = async function (context, req) {
         context.res = {
             status: 400,
             body: "No body in request"
-        };        
+        };
+        return;
     }
-    else if (!req.body.bundleSize) {
+
+    const err = checkDataContainsFields(req.body, mandatoryFields);
+    if (err !== undefined) {
+        context.log('Error:', err);
         context.res = {
             status: 400,
-            body: "No bundleSize in request"
-        };        
-    }
-    else if (req.query.name || (req.body && req.body.name)) {
-
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
+            body: err
         };
-
-        const mongoClient = await mongodb.MongoClient.connect(mongodbUri, {
-            auth: {
-                user: mongodbUser,
-                password: mongodbPass
-            }
-        });
-
-        context.log('connected to mongodb')
-        await mongoClient.db('stardust').collection('stats').insertOne({
-            name: req.body.name,
-            bundleSize: req.body.bundleSize
-        });
-        context.log('document inserted')
-        await mongoClient.close()
+        return;
     }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-    }
+
+    const mongoClient = await mongodb.MongoClient.connect(mongodbUri, {
+        auth: {
+            user: mongodbUser,
+            password: mongodbPass
+        }
+    });
+
+    context.log('connected to mongodb')
+    await mongoClient.db('stardust').collection('stats').insertOne(_.pick(req.body, [...mandatoryFields, ...optionalFields]));
+    context.log('document inserted')
+    await mongoClient.close()
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: `Stats saved`
+    };
 };
